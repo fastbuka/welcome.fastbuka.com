3262985033ea2d3333cb86f7bf54a93fb0554b9b
Progress Ochuko Eyaadah <106968712+KoxyG@users.noreply.github.com>
2024-11-17 09:57:14 +0100

    Update Faq.tsx

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -1,8 +1,8 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/component/accordion"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../component/accordion"
   
   export default function FastbukaFAQ() {
     return (
-      <div className="container mx-auto px-[20px] sm:px-[150px] py-[100px]">
+      <div className="container mx-auto px-[20px] sm:px-[200px] py-[100px]">
         <h2 className="text-3xl font-bold text-center mb-6">Frequently Asked Questions ?</h2>
         <Accordion type="single" collapsible className="w-full">
           <AccordionItem value="item-1">
